31c5f6cbf426e5149b5f38c76f1dc1da7f78e963
Shahrukh M <shahrukhmx@gmail.com>
2017-04-01 10:19:47 -0500

    Removed comments from backend and added as resource to frontend files.

--- backend.js
+++ backend.js
@@ -1,15 +1,15 @@
-{/*<script src="https://www.gstatic.com/firebasejs/3.7.0/firebase.js"></script>*/}
-{/*<script>*/}
-  // Initialize Firebase
-  // var config = {
-  //   apiKey: "AIzaSyB7Y5DCEpGeuz37UwuD0humCAHUOW2uJI8",
-  //   authDomain: "hear-9083d.firebaseapp.com",
-  //   databaseURL: "https://hear-9083d.firebaseio.com",
-  //   storageBucket: "hear-9083d.appspot.com",
-  //   messagingSenderId: "766933243175"
-  // };
-  // firebase.initializeApp(config);
-// </script>
+//Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB7Y5DCEpGeuz37UwuD0humCAHUOW2uJI8",
+  authDomain: "hear-9083d.firebaseapp.com",
+  databaseURL: "https://hear-9083d.firebaseio.com",
+  storageBucket: "hear-9083d.appspot.com",
+  messagingSenderId: "766933243175"
+};
+
+firebase.initializeApp(config);
+//geolocation();
+getChatRooms();
 
 var database = firebase.database();
 var userId = 0;
@@ -29,8 +29,9 @@ function userLogin(userId, name, range = 10) {
 
 //Once logged in, get the chat rooms
 function getChatRooms(){
-  return firebase.database().ref('/chatRooms/').once('value').then(function(snapshot) {
+  return firebase.database().ref('/Messages/').once('value').then(function(snapshot) {
   var chatRooms = snapshot.val().chatRooms;
+  console.log(chatRooms);
 });
 }
 
@@ -46,4 +47,47 @@ function createChatRoom(title, userId, maxNumUsers=10, chatRoomID){
 
 function setRange(givenRange){
   userRange=givenRange;
+}
+
+function geolocation() {
+  if (navigator.geolocation) {
+  console.log('Geolocation is supported!');
+  }
+  else {
+    console.log('Geolocation is not supported for this Browser/OS.');
+  }
+
+  var startPos;
+  var nudge = document.getElementById("nudge");
+
+  var showNudgeBanner = function() {
+    nudge.style.display = "block";
+  };
+
+  var hideNudgeBanner = function() {
+    nudge.style.display = "none";
+  };
+
+  var nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
+
+  var geoSuccess = function(position) {
+    hideNudgeBanner();
+    // We have the location, don't display banner
+    clearTimeout(nudgeTimeoutId);
+
+    // Do magic with location
+    startPos = position;
+    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+  };
+  var geoError = function(error) {
+    switch(error.code) {
+      case error.TIMEOUT:
+        // The user didn't accept the callout
+        showNudgeBanner();
+        break;
+    }
+  };
+
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
 }
