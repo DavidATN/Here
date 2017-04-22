97bc721a25abca639f9d5be563c0c31cf53c133c
McQuain <linscot7@uwm.edu>
2017-04-22 14:32:18 -0500

    Did backend stuff for send message and retrieval

--- messages.js
+++ messages.js
@@ -1,8 +1,8 @@
 function publishMessages(childData) {
   var e = $("#messages");
   for (var i=0; i < childData.messages.length; i++) {
-    $("<p>Name Goes Here</p>").appendTo(e[0]);
-    var m = "<div class='chat-body1 clearfix'><p>" + childData.messages[i] + "</p><div class='chat_time pull-right'>09:40PM</div></div>";
+    $("<p>"+childData.messages[i][0]+ "</p>").appendTo(e[0]);
+    var m = "<div class='chat-body1 clearfix'><p>" + childData.messages[i][1] + "</p><div class='chat_time pull-right'>09:40PM</div></div>";
     $(m).appendTo(e[0]);
   }
 }
